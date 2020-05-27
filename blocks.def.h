bc3eaa5f919d03cf5a2a7cb8877b1dd58c7feb0b
Yiqun Ding <yiqunding@zju.edu.cn>
2020-05-27 16:37:47 +0800

    apply dwmblocks-statuscmd.diff
    it's not a official dwmblocks patch, it's from a dwm patch
    it allows different blocks to handle signal from dwm differently
    ref: https://dwm.suckless.org/patches/statuscmd/
    
    add the patch file

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	/*Icon*/   /*Command*/   /*Update Interval*/   /*Update Signal*/
+	{"",       "weather",    600,                  5},
+	{"",       "mailbox",    0,                    12},
+	{"",       "cputemp",    5,                    21},
+	{"",       "memory",     2,                    22},
+	{"",       "connstatus", 0,                    4},
+	{"",       "volume",     0,                    10},
+	{"",       "battery",    5,                    23},
+	{"",       "clock",      30,                   24},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "\0";
 static unsigned int delimLen = 5;
