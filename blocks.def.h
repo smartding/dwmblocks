dc320fba749803a3cca1af7052dcda973faac703
Yiqun Ding <yiqunding@zju.edu.cn>
2024-05-29 15:06:10 +0800

    add .sh to scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"",        "weather",      3600,                             5},
-    {"",        "upable",       0,                               25},
-    {"",        "mailbox",      0,                               12},
-    {"",        "cputemp",      5,                               21},
-    {"",        "memory",       2,                               22},
-    {"",        "ifstatus",     0,                                4},
-    {"",        "volume",       0,                               10},
-    {"",        "battery",      5,                               23},
-    {"",        "clock",        2,                               24},
+	/*Icon*/    /*Command*/        /*Update Interval*/              /*Update Signal*/
+    {"",        "weather.sh",      3600,                            5},
+    {"",        "ups.sh",          0,                               25},
+    {"",        "mailbox.sh",      0,                               12},
+    {"",        "cputemp.sh",      5,                               21},
+    {"",        "memory.sh",       2,                               22},
+    {"",        "ifstatus.sh",     0,                                4},
+    {"",        "volume.sh",       0,                               10},
+    {"",        "battery.sh",      5,                               23},
+    {"",        "clock.sh",        2,                               24},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
